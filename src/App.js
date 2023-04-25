be238f57c75c4faee683c2ed1fcc4289431c671b
Jean Campelo <jean93campelo@gmail.com>
2023-04-25 15:33:43 -0300

    config react

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import GlobalStyles from "./assets/GlobalStyles";
+
+export default function App () {
+    return (
+        <BrowserRouter>
+        <GlobalStyles />
+        initial screen
+        </BrowserRouter>
+    )
+}
